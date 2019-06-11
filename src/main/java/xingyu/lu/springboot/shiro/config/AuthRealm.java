8c243b188b436b5d17f13a50683aff32bc3d9c61
LoseyourseIf <15025160469@163.com>
2019-06-11 11:20:22 +0800

    Component

--- src/main/java/xingyu/lu/springboot/shiro/config/AuthRealm.java
+++ src/main/java/xingyu/lu/springboot/shiro/config/AuthRealm.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import xingyu.lu.springboot.shiro.domain.UserExt;
 import xingyu.lu.springboot.shiro.service.UserService;
 import xingyu.lu.springboot.shiro.utils.jwt.JwtClaimGetter;
